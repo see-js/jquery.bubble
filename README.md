c5d8d51dddfb914713ed3b26eab4d7a15eb26a2b
zhengwujiang <cdzhengwujiang@jd.com>
2017-01-11 11:19:27 +0800

    提交气泡自由落体插件

---
+++ README.md
@@ -0,0 +1,46 @@
+## jquery.bubble
+
+一个基于jQuery的气泡动画插件，在指定区域上方（左/右）定时间隔产生气泡，然后随机水平速度进行仿自由落体运动。到达区域底部之后做弹跳运动，速度逐渐衰减。直至停止运动。
+
+[在线演示](http://seejs.me/jquery.bubble/demo/)
+
+### 安装方法
+
+由于是基于jQuery的扩展插件，因此引入jQuery是必须的。
+此外，还需引入插件自身的实现脚本。
+
+```xml
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="../js/jquery.bubble.min.js"></script>
+```
+
+在此之前，我们还需要引入样式依赖文件`jquery.bubble.min.css`，主要包含气泡自身基础样式。
+
+```css
+<link rel="stylesheet" href="../css/jquery.bubble.min.css">
+```
+
+### 使用示例
+
+插件会在指定的元素范围内运行，如果找不到指定的元素，插件将以body元素作为容器。
+
+```js
+$(function() {
+    $('#J_PluginWrap').bubble({
+        // 气泡元素Class
+        itemClass: 'J_BubbleItem',
+        // 气泡大小范围[最小值, 最大值]，单位px
+        size: [60, 120],
+        // 气泡吹大时间范围[最小值, 最大值]，单位s
+        blowTime: [0.5, 1],
+        // 气泡出现的方向，left/right
+        direction: 'right',
+        // 位置偏移量，[x, y]，效果与 direction 相关
+        offset: [0, 60],
+        // 气泡产生时间间隔，单位s
+        interval: 1.2
+    });
+});
+```
+
+更多详细信息，请参看[源码](https://github.com/mailzwj/jquery.bubble)。
---
+++ js/jquery.bubble.js
@@ -0,0 +1,177 @@
+$(function() {
+    var _DEFAULT = {
+        // 气泡元素Class
+        itemClass: 'J_BubbleItem',
+        // 气泡大小范围[最小值, 最大值]，单位px
+        size: [60, 120],
+        // 气泡吹大时间范围[最小值, 最大值]，单位s
+        blowTime: [0.5, 1],
+        // 气泡出现的方向，left/right
+        direction: 'right',
+        // 位置偏移量，[x, y]，效果与 direction 相关
+        offset: [0, 60],
+        // 气泡产生时间间隔，单位s
+        interval: 1.2
+    };
+
+    var DIRFLAG = {
+        left: true,
+        right: true
+    };
+
+    function ImPluginClass(wrap, cfg) {
+        var config = $.extend(true, {}, cfg || {});
+
+        this.wrap = wrap;
+        this.get = function(n) {
+            return config[n];
+        };
+        this.set = function(n, v) {
+            config[n] = v;
+        };
+
+        this.init();
+    }
+
+    ImPluginClass.prototype.init = function() {
+        // 格式化气泡容器
+        this.formatWrap();
+        // 启动动画
+        this.blow();
+    };
+
+    ImPluginClass.prototype.formatWrap = function() {
+        var pos = this.wrap.css('position');
+        if (!pos || pos === 'static') {
+            this.wrap.css('position', 'relative');
+        }
+    };
+
+    ImPluginClass.prototype.blow = function() {
+        var that = this;
+        var wrap = that.wrap;
+        var timer = that.get('timer');
+        if (timer) {
+            clearInterval(timer);
+        }
+        timer = setInterval(function() {
+            var item = $('<div class="' + that.get('itemClass') + ' bubbles">');
+            var size = that.get('size');
+            var dir = that.get('direction');
+            var offset = that.get('offset');
+            var width = (size[1] - size[0]) * Math.random() + size[0];
+            var bTime = that.get('blowTime');
+            var blowTime = (bTime[1] - bTime[0]) * Math.random() + bTime[0];
+            var toTop = offset[1] - width / 2;
+            var style = {
+                position: 'absolute',
+                width: 0,
+                height: 0
+            };
+
+            if (!DIRFLAG[dir]) {
+                dir = 'left';
+            }
+
+            style[dir] = offset[0];
+            style['top'] = offset[1];
+
+            item.css(style);
+            wrap.append(item);
+            item.animate({
+                top: toTop,
+                width: width,
+                height: width
+            }, blowTime * 1000, function() {
+                setTimeout(function() {
+                    that.fall.call(that, item);
+                }, 150);
+            });
+            // clearInterval(timer);
+        }, that.get('interval') * 1000);
+        that.set('timer', timer);
+    };
+
+    ImPluginClass.prototype.fall = function(item) {
+        var that = this;
+        var dir = that.get('direction');
+        var wrapWdith = that.wrap.width();
+        var wrapHeight = that.wrap.height();
+        var itemWidth = item.outerWidth();
+        var itemHeight = item.outerHeight();
+        var speedx = Math.random() * wrapWdith * 0.1 + 10;
+        var speedy = 0;
+        var decay = Math.random() * 0.4 + 0.6;
+        var startx;
+        var starty;
+        var endPoint;
+        var interval;
+        speedx = dir === 'right' ? speedx * -1 : speedx;
+        interval = setInterval(function() {
+            var nT;
+            var nL;
+            startx = parseFloat(item.css('left'));
+            starty = parseFloat(item.css('top'));
+            speedy += 3;
+            nT = starty + speedy;
+            nL = startx + speedx;
+
+            if (nT < 0) {
+                nT = 0;
+                speedy *= -1 * decay;
+                speedy < 0 ? Math.ceil(speedy) : Math.floor(speedy);
+                speedx *= decay;
+                speedx < 0 ? Math.ceil(speedx) : Math.floor(speedx);
+            } else if (nT > wrapHeight - itemHeight) {
+                nT = wrapHeight - itemHeight;
+                speedy *= -1 * decay;
+                speedy < 0 ? Math.ceil(speedy) : Math.floor(speedy);
+                speedx *= decay;
+                speedx < 0 ? Math.ceil(speedx) : Math.floor(speedx);
+            }
+
+            if (nL < 0) {
+                nL = 0;
+                speedx *= -1 * decay;
+                speedx < 0 ? Math.ceil(speedx) : Math.floor(speedx);
+            } else if (nL > wrapWdith - itemWidth) {
+                nL = wrapWdith - itemWidth;
+                speedx *= -1 * decay;
+                speedx < 0 ? Math.ceil(speedx) : Math.floor(speedx);
+            }
+
+            item.css({
+                left: nL,
+                top: nT
+            });
+
+            if (nT === endPoint) {
+                clearInterval(interval);
+                setTimeout(function() {
+                    that.remove.call(that, item);
+                }, 300);
+            }
+            endPoint = nT;
+        }, 25);
+    };
+
+    ImPluginClass.prototype.remove = function(item) {
+        item.fadeOut(function() {
+            item.remove();
+        });
+    };
+
+    $.fn.bubble = function(cfg) {
+        var config = $.extend({}, _DEFAULT, cfg || {});
+        if (!this.length) {
+            throw('Elements not found.');
+        }
+        return this.each(function(idx, ele) {
+            var $this = $(ele);
+            if (!$this.parents('body').length) {
+                $this = $('body');
+            }
+            $this.data('bubble', new ImPluginClass($this, config));
+        });
+    };
+});
